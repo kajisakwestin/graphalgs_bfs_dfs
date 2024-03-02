e3b0c41399d53b16a121a5a1543e0d1c8b63717f
kajwestin <kaj.westin@dataductus.se>
2024-03-02 18:29:24 +0100

    testing different graphs

--- graphalgs_bfs_dfs/graphalgs_bfs_dfs/Program.cs
+++ graphalgs_bfs_dfs/graphalgs_bfs_dfs/Program.cs
@@ -1,18 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using graphalgs_bfs_dfs;
 
-Graph g = new Graph(4);
+int numberOfNodes = 5;
+Graph g = new Graph(numberOfNodes);
+int startNode;
+
+//g.AddEdge(0, 1);
+//g.AddEdge(0, 2);
+//g.AddEdge(1, 2);
+//g.AddEdge(2, 0);
+//g.AddEdge(2, 3);
+//g.AddEdge(3, 3);
 
 g.AddEdge(0, 1);
 g.AddEdge(0, 2);
-g.AddEdge(1, 2);
-g.AddEdge(2, 0);
-g.AddEdge(2, 3);
+g.AddEdge(0, 3);
+g.AddEdge(0, 4);
+g.AddEdge(1, 1);
+g.AddEdge(2, 2);
 g.AddEdge(3, 3);
+g.AddEdge(4, 4);
 
-Console.WriteLine("BFS från nod 2:");
-g.BFS(2);
-Console.WriteLine("\nDFS från nod 2:");
-g.DFS(2);
+//ta emot ett värde att använda som startnod
+Console.WriteLine("Ange startnod:");
+startNode = int.Parse(Console.ReadLine());
 
+//så länge inte startNode är negativt eller större än antalet noder, fortsätt fråga efter startnod
+while (startNode >= 0 && !(startNode > numberOfNodes - 1))
+{
+    Console.WriteLine($"BFS från nod {startNode}:");
+    g.BFS(startNode);
+    Console.WriteLine($"\nDFS från nod {startNode}:");
+    g.DFS(startNode);
+
+    Console.WriteLine("\n\nAnge en ny startnod:");
+    startNode = int.Parse(Console.ReadLine());
+}
+
+//avsluta programmet
+Console.WriteLine("The End");
 Console.ReadLine();
