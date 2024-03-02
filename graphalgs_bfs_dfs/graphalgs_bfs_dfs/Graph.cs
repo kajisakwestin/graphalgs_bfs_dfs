dd8a773d7f9741172c07389e7e7e9df6d069bff2
kajwestin <kaj.westin@dataductus.se>
2024-03-02 17:22:31 +0100

    port from .net framework to .net8

---
+++ graphalgs_bfs_dfs/graphalgs_bfs_dfs/Graph.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graphalgs_bfs_dfs
+{
+    class Graph
+    {
+        private int V; // Antal noder
+        private List<int>[] adj; // Adjacencylista
+
+        public Graph(int v)
+        {
+            V = v;
+            adj = new List<int>[v];
+            for (int i = 0; i < v; ++i)
+                adj[i] = new List<int>();
+        }
+
+        public void AddEdge(int v, int w)
+        {
+            adj[v].Add(w); // Lägg till w till v's lista.
+        }
+
+        // BFS från en given startnod
+        public void BFS(int s)
+        {
+            bool[] visited = new bool[V]; // Alla noder är inte besökta i början
+
+            Queue<int> queue = new Queue<int>(); // Skapa en kö för BFS
+
+            visited[s] = true; // Markera aktuell nod som besökt och lägg till den i kön
+            queue.Enqueue(s);
+
+            while (queue.Count != 0)
+            {
+                s = queue.Dequeue(); // Ta bort en nod från kön och skriv ut den
+                Console.Write(s + " ");
+
+                // Hämta alla grannar till den borttagna noden och om en granne inte är besökt, markera den som besökt och lägg till den i kön
+                foreach (int next in adj[s])
+                {
+                    if (!visited[next])
+                    {
+                        visited[next] = true;
+                        queue.Enqueue(next);
+                    }
+                }
+            }
+        }
+
+        // DFS från en given startnod
+        public void DFS(int s)
+        {
+            bool[] visited = new bool[V]; // Alla noder är inte besökta i början
+
+            Stack<int> stack = new Stack<int>(); // Skapa en stack för DFS
+
+            visited[s] = true; // Markera aktuell nod som besökt och lägg till den i stacken
+            stack.Push(s);
+
+            while (stack.Count != 0)
+            {
+                s = stack.Pop(); // Ta bort en nod från stacken och skriv ut den
+                Console.Write(s + " ");
+
+                // Hämta alla grannar till den borttagna noden och om en granne inte är besökt, markera den som besökt och lägg till den i stacken
+                foreach (int next in adj[s])
+                {
+                    if (!visited[next])
+                    {
+                        visited[next] = true;
+                        stack.Push(next);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ graphalgs_bfs_dfs/graphalgs_bfs_dfs/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using graphalgs_bfs_dfs;
+
+Console.WriteLine("Hello, World!");
+
+Graph g = new Graph(4);
+
+g.AddEdge(0, 1);
+g.AddEdge(0, 2);
+g.AddEdge(1, 2);
+g.AddEdge(2, 0);
+g.AddEdge(2, 3);
+g.AddEdge(3, 3);
+
+Console.WriteLine("BFS från nod 2:");
+g.BFS(2);
+Console.WriteLine("\nDFS från nod 2:");
+g.DFS(2);
+
+Console.ReadLine();
